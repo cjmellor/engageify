5ba748ef30630f85509dd9134fc413d8610291ef
Chris Mellor <cmellor@gmail.com>
2023-10-07 17:38:44 +0100

    feat: Add config options

--- config/engageify.php
+++ config/engageify.php
@@ -14,4 +14,25 @@ return [
         'model' => App\Models\User::class,
         'table' => 'users',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Multiple Engagements
+    |--------------------------------------------------------------------------
+    |
+    | Allow multiple engagements of the same type.
+    |
+    */
+    'allow_multiple_engagements' => env(key: 'ENGAGEIFY_MULTIPLE_ENGAGEMENTS', default: false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allow Caching
+    |--------------------------------------------------------------------------
+    |
+    | The engagement counts can be cached to improve performance.
+    |
+    */
+    'allow_caching' => env(key: 'ENGAGEIFY_ALLOW_CACHING', default: false),
+    'cache_duration' => env(key: 'ENGAGEIFY_CACHE_DURATION', default: 3600),
 ];
