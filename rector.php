2767d645a9f834dfb3fd0d1029f5ad979d41b96c
Chris Mellor <chris@mellor.pizza>
2026-05-25 17:37:38 +0100

    build(deps): Add Rector, Larastan and Pest type-coverage tooling
    Bring in the dev tooling used by the level-up package:
    
    - require-dev: rector/rector, driftingly/rector-laravel, larastan/larastan,
      pestphp/pest-plugin-type-coverage
    - Add rector.php (LaravelSetProvider + composer-based version detection,
      plus the code-quality/collection/if-helper sets) and phpstan.neon
      (Larastan, level 5)
    - Scripts: add format:dry, test-coverage and test:type-coverage (both
      enforcing --min=100); test now runs Pest
    - Move minimum-stability/prefer-stable to the root (they were nested under
      "extra" where Composer ignored them)
    - Drop the unused "Engageify" facade alias

---
+++ rector.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
+use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withSkip([AddOverrideAttributeToOverriddenMethodsRector::class])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+    )
+    ->withPhpSets(php83: true)
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true)
+    ->withSets([
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+    ])
+    ->withConfiguredRule(RemoveDumpDataDeadCodeRector::class, ['dd', 'dump']);
